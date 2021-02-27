48d07c7d59f3a9dfcde3f5b2cd4e99da2f83391e
André Pontes <33063611+Pottzork@users.noreply.github.com>
2021-02-27 18:31:01 +0100

    Add existing project

---
+++ Project MUSK/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+
+    [SerializeField] float thrustSpeed = 100f;
+    [SerializeField] float rotationThrust = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; //Freezing rotation so that we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+        } 
+    }
+}
